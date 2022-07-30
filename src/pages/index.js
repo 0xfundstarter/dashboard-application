91b3688d07f49fb3fcc67e65ee74b2bb1bce44f7
Shikhar Singh <64684053+linux369@users.noreply.github.com>
2022-07-30 23:34:36 +0530

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,33 @@
+import BannerSlider from "@/components/BannerSlider/BannerSlider";
+import BrandArea from "@/components/BrandArea/BrandArea";
+import Categories from "@/components/Categories/Categories";
+import CtaArea from "@/components/CtaArea/CtaArea";
+import FunFacts from "@/components/FunFacts/FunFacts";
+import GuideArea from "@/components/GuideArea/GuideArea";
+import Header from "@/components/Header/Header";
+import Layout from "@/components/Layout/Layout";
+import NewsArea from "@/components/NewsArea/NewsArea";
+import ProjectsArea from "@/components/ProjectsArea/ProjectsArea";
+import TeamArea from "@/components/TeamArea/TeamArea";
+import TeamMainArea from "@/components/TeamArea/TeamMainArea";
+import React from "react";
+
+const Home = () => {
+  return (
+    <Layout>
+      <Header />
+      <BannerSlider />
+      <Categories />
+      <CtaArea />
+      <ProjectsArea />
+      <FunFacts />
+      <BrandArea />
+      <TeamArea />
+      <TeamMainArea />
+      <NewsArea />
+      <GuideArea />
+    </Layout>
+  );
+};
+
+export default Home;
