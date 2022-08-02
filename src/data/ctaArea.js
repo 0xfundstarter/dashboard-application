65207f2de76cc111acb4a4ec551a1fa338dcd8b8
0xshikhar <shikhar@iiitmanipur.ac.in>
2022-08-03 01:04:04 +0530

    update

--- src/data/ctaArea.js
+++
@@ -1,6 +0,0 @@
-import bg from "@/images/cta-bg.jpg";
-
-export const ctaArea = {
-  bg,
-  title: "Ready to Raise Funds for Idea?",
-};
--- src/data/guideArea.js
+++
@@ -1,7 +0,0 @@
-import bg from "@/images/guide-bg.jpg";
-
-export const guideArea = {
-  bg,
-  tagline: "Get Our Complete",
-  title: "Crowdfunding Guide",
-};
