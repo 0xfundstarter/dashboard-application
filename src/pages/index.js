6a270670b19674e0d52d7d99f08e42a75ae07795
0xshikhar <shikhar@iiitmanipur.ac.in>
2022-08-06 01:30:51 +0530

    adding project pages

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,17 @@
-import BannerSlider from "@/components/BannerSlider/BannerSlider";
-import BrandArea from "@/components/BrandArea/BrandArea";
-import Categories from "@/components/Categories/Categories";
-import CtaArea from "@/components/CtaArea/CtaArea";
-// import FunFacts from "@/components/FunFacts/FunFacts";
-import GuideArea from "@/components/GuideArea/GuideArea";
+// import BannerSlider from "@/components/BannerSlider/BannerSlider";
+// import BrandArea from "@/components/BrandArea/BrandArea";
+// import Categories from "@/components/Categories/Categories";
+// import CtaArea from "@/components/CtaArea/CtaArea";
+// // import FunFacts from "@/components/FunFacts/FunFacts";
+// import GuideArea from "@/components/GuideArea/GuideArea";
+
+// import NewsArea from "@/components/NewsArea/NewsArea";
+// import TeamArea from "@/components/TeamArea/TeamArea";
+// import TeamMainArea from "@/components/TeamArea/TeamMainArea";
+
+import ProjectsArea from "@/components/ProjectsArea/ProjectsArea";
 import Header from "@/components/Header/Header";
 import Layout from "@/components/Layout/Layout";
-import NewsArea from "@/components/NewsArea/NewsArea";
-import ProjectsArea from "@/components/ProjectsArea/ProjectsArea";
-import TeamArea from "@/components/TeamArea/TeamArea";
-import TeamMainArea from "@/components/TeamArea/TeamMainArea";
 import React from "react";
 
 const Home = () => {
